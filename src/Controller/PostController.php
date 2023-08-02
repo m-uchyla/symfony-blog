e4838719001c64b6fb90ad7e0e369638a2b55f24
m-uchyla <czarnytroll@gmail.com>
2023-08-02 14:19:35 +0200

    Controllers paths rename

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
+    #[Route('/post', name: 'post')]
     public function index(): Response
     {
         return $this->render('post/index.html.twig', [
